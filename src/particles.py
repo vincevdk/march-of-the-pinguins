33ad2ad50c423af960a15fa72670cef72a9b4596
Vincent van de Kerkhof <v.vandekerkhof@student.tudelft.nl>
2019-05-23 14:39:36 +0200

    added input N_particles

--- src/particles.py
+++ src/particles.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def particle(a, b, r):
+def particle(a, b, r, N_particles):
     # (a,b): the center of the circle
     # r: the radius of the circle 
     # T: The number of the segments                                   
@@ -8,7 +8,7 @@ def particle(a, b, r):
     x = np.zeros((len(r),T))
     y =  np.zeros((len(r),T))
     theta = (np.linspace(0,2*np.pi,T))
-    theta = np.tile(theta,(25,1)).T
+    theta = np.tile(theta,(N_particles,1)).T
     x = a + np.multiply(r,np.cos(theta))
     y = b + np.multiply(r,np.sin(theta))
     return x, y
